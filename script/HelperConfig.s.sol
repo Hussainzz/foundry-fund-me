274b817cde390396566f452a85e5b8f5a416ee61
Hussain'z <shkhussain12@gmail.com>
2023-06-11 12:10:22 +0530

    💸 Foundry 🔥

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script{
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor(){
+        if(block.chainid == 11155111){
+            activeNetworkConfig = getSepoliaEthConfig();
+        }else{
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns(NetworkConfig memory sepoliaConfig){
+        //price feed address
+        sepoliaConfig = NetworkConfig({
+            priceFeed:0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory anvilConfig){
+        if(activeNetworkConfig.priceFeed != address(0)){
+            return activeNetworkConfig;
+        }
+        //Deploy priceFeed mocks
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+        anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+    }
+}
